b7e33ac174f69fdf153d9ba1df003a3debf3aa1c
goldandrei <goldenbergandrei@gmail.com>
2023-08-22 14:23:00 +0300

    first commit

---
+++ pages/search/[query].js
@@ -0,0 +1,19 @@
+import Layout from "../../components/Layout"
+import List from "../../components/List"
+import { search } from "../api"
+
+export default function News(props) {
+    return(
+      <Layout>
+        <List {...props} />
+      </Layout>
+    )
+}
+
+const API_KEY = "9hUvOqGGdnCBvGKg4EB3L7mGdBC8hKKJ"
+export async function getServerSideProps({  params }) {
+  const results = await search(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${params.query}&api-key=${API_KEY}`
+  )
+  return { props: { results } }
+
+}
